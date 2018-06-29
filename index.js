b48ca2068d75237fae8eb0403c51acfadcaa0b3f
sves <33582501+svestol@users.noreply.github.com>
2018-06-29 10:39:26 +0200

    some debugging

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
+  console.log(req)
   res.json({ hello: 'world' })
 })
 
 app.listen(8080, () => {
   console.log('running at ', 8080)
+
+  console.log(process.env)
 })
